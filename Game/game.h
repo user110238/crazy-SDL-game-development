b93d1324fe791a2854b2083e15b98282066005a7
User <user110248@gmail.com>
2024-01-22 17:26:10 +0100

    22/01

--- Game/game.h
+++ Game/game.h
@@ -1,42 +1,42 @@
     // Creates a window and gives ScreenSurface it's surface
-bool init( SDL_Window** Window , SDL_Surface** ScreenSurface , const int WindowWidth , const int WindowHeight );
+bool init( SDL_Window** Window , SDL_Surface** ScreenSurface , SDL_Renderer** Renderer , const int WindowWidth , const int WindowHeight );
     // Gives a SDL_Surface an image
 bool LoadMedia(SDL_Surface** Surface , std::string path);
     // Free-s up window and it's surface memory
-void close(SDL_Surface* Background , SDL_Window* Window);
+void close( SDL_Window* Window);
 
-bool init( SDL_Window** Window , SDL_Surface** ScreenSurface , const int WindowWidth , const int WindowHeight )
+bool init( SDL_Window** Window , SDL_Surface** ScreenSurface , SDL_Renderer** Renderer, const int WindowWidth , const int WindowHeight )
 {
 
     bool check = 1;
 
     *Window = SDL_CreateWindow( "Schizo Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WindowWidth, WindowHeight, SDL_WINDOW_SHOWN );
+
     if( Window == NULL )
     {
         std::cout << " Window not created " << SDL_GetError();
     }
         else
     {
+        *Renderer = SDL_CreateRenderer( *Window, -1, SDL_RENDERER_ACCELERATED );
+        SDL_SetRenderDrawColor( *Renderer, 0xFF, 0xFF, 0xFF, 0xFF );
         *ScreenSurface = SDL_GetWindowSurface( *Window );
     }
 
     return check;
 }
 
-bool LoadMedia(SDL_Surface** Background , std::string path)
+bool LoadMedia(SDL_Surface** Surface , std::string path )
 {
     bool check = 1;
 
-    *Background = SDL_LoadBMP( path.c_str() );
+    *Surface = SDL_LoadBMP( path.c_str() );
 
     return check;
 }
 
-void close(SDL_Surface* Background , SDL_Window* Window)
+void close( SDL_Window* Window)
 {
-        // Free Surface
-    SDL_FreeSurface( Background );
-    Background = NULL;
 
         // Free Window
     SDL_DestroyWindow( Window );
--- Game/main.cpp
+++ Game/main.cpp
@@ -5,31 +5,69 @@
     // Init , LoadMedia , close
 #include "game.h"
 
+
 int main( int argc, char* args[] )
 {
     const int WindowWidth = 1600;
     const int WindowHeight = 800;
 
+    bool quit = false;
 
         // Window Pointers
     SDL_Window* Window = NULL;
     SDL_Surface* ScreenSurface = NULL;
     SDL_Surface* Background = NULL;
+        // Window Renderer
+    SDL_Renderer* Renderer = NULL;
+        // Event Pointers
+    SDL_Event event;
 
-    if ( !init(&Window , &ScreenSurface , WindowWidth , WindowHeight) )
+        // Initialization
+    if ( !init(&Window , &ScreenSurface , &Renderer , WindowWidth , WindowHeight) )
         std::cout << " Init fail ";
     if ( !LoadMedia( &Background , "Images/idk.bmp") )
         std::cout << " Load background fail ";
 
-    SDL_BlitSurface( Background, NULL, ScreenSurface, NULL );
+        // Game loop
+    while ( !quit )
+    {
+            // Event loop
+         while( SDL_PollEvent( &event ) != 0 )
+                {
+                    //User requests quit
+                    switch ( event.type )
+                    {
+                            // Quit event
+                        case SDL_QUIT:
+                            quit = true;
+                            break;
+                            // Detects key
+                        case SDL_KEYDOWN:
+                                // Key events
+                            switch ( event.key.keysym.sym )
+                            {
+                                case SDLK_w:
+
+
+                                case SDLK_s:
+
+                                    break;
+
+                            }
+
+                            break;
+
+                        }
+                }
+        }
+
+        /*SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );
+        SDL_RenderFillRect( gRenderer, &fillRect );*/
+
     SDL_UpdateWindowSurface( Window );
 
-
-
-
-    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-
-    close( Background , Window );
+    close( Window );
 
     return 0;
 }
